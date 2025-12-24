eecdcb0fdfbbf3cf2541f7d4a6f0c61b27444c37
morrowchristian <morrowchristian@icloud.com>
2025-12-23 21:51:23 -0500

    refactor(app): simplify App layout for snap-scroll panel architecture

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,22 @@ import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
-import DarkModeToggle from "./components/DarkModeToggle";
 
-function App() {
+const App = () => {
   return (
-    <div className="app-container">
+    <>
       <Navbar />
-      <DarkModeToggle />
+
       <Hero />
       <About />
       <Skills />
       <Projects />
       <Contact />
-      <Footer />
+
       <ScrollToTop />
-    </div>
+    </>
   );
-}
+};
 
 export default App;
